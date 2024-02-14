91095401c1865cfc966c27e59682c511b7bcda11
kevin4moe <kevin4moe@gmail.com>
2024-02-14 15:31:16 -0600

    cloud 0.3

--- netlify/functions/db/index.js
+++ netlify/functions/db/index.js
@@ -3,15 +3,28 @@ import { MongoClient, ServerApiVersion } from 'mongodb';
 
 // Connection URI
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v8usw.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
+
+let cachedDb = null;
+
+async function connectToDatabase() {
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  if (cachedDb) {
+    return cachedDb;
   }
-});
+
+  await client.connect();
+  const db = client.db(process.env.DB_NAME);
+  cachedDb = db;
+  return db
+}
 
 async function run(next) {
   try {
@@ -31,17 +44,11 @@ async function run(next) {
 
 export const handler = async function (event = {}, context = {}) {
 
+  const db = await connectToDatabase();
+
   const groupUnits = event.body
     ? JSON.parse(event.body)
-    : [{
-      id: "b871487c-71ab-4f02-b284-274444cfde4d",
-      name: "a",
-      category: "Alimentos",
-      unit: "kg",
-      quantity: 2,
-      price: 25,
-      total: 50,
-    }];
+    : [];
 
   groupUnits.items.forEach((item, i) => {
     if (item.price) {
@@ -50,26 +57,40 @@ export const handler = async function (event = {}, context = {}) {
     groupUnits.items[i].total = Number(item.total.replace(/[^0-9.-]+/g, ""));
   });
 
+  // const collectionUnits = db.collection(process.env.COLLECTION_UNITS);
+  const collectionGroup = db.collection(process.env.COLLECTION_GROUP);
 
-  run(async (db) => {
-    const collectionUnits = db.collection(process.env.COLLECTION_UNITS);
-    const collectionGroup = db.collection(process.env.COLLECTION_GROUP);
+  // const cu = await collectionUnits.insertMany(groupUnits.items);
+  const cg = await collectionGroup.insertOne({
+    "id": "b871487c-71ab-4f02-b284-274444cfde4d",
+    "products_ids": groupUnits.items.map((pr) => pr.ids ? pr.ids : pr.id),
+    "date": new Date(),
+    "state": "Guanajuato",
+    "county": "Guanajuato",
+    "user_id": "4moe",
+  });
 
-    const cu = await collectionUnits.insertMany(groupUnits.items);
-    const cg = await collectionGroup.insertOne({
-      "id": "b871487c-71ab-4f02-b284-274444cfde4d",
-      "products_ids": groupUnits.items.map((pr) => pr.ids ? pr.ids : pr.id),
-      "date": new Date(),
-      "state": "Guanajuato",
-      "county": "Guanajuato",
-      "user_id": "4moe",
-    });
+  console.log(cu, cg);
 
-    console.log(cu, cg);
+  // run(async (db) => {
+  //   const collectionUnits = db.collection(process.env.COLLECTION_UNITS);
+  //   const collectionGroup = db.collection(process.env.COLLECTION_GROUP);
 
-  }).catch(console.dir);
+  //   // const cu = await collectionUnits.insertMany(groupUnits.items);
+  //   const cg = await collectionGroup.insertOne({
+  //     "id": "b871487c-71ab-4f02-b284-274444cfde4d",
+  //     "products_ids": groupUnits.items.map((pr) => pr.ids ? pr.ids : pr.id),
+  //     "date": new Date(),
+  //     "state": "Guanajuato",
+  //     "county": "Guanajuato",
+  //     "user_id": "4moe",
+  //   });
 
-  console.log(event.body, event)
+  //   console.log(cu, cg);
+
+  // }).catch(console.dir);
+
+  console.log(event.multiValueHeaders)
 
   return {
     headers: {
@@ -77,7 +98,6 @@ export const handler = async function (event = {}, context = {}) {
       // "Content-Type": "application/pdf",
     },
     statusCode: 200,
-    // body: body64.split(",")[1],
-    // isBase64Encoded: true,
+    body: JSON.stringify({ message: "Noice" })
   };
 }
